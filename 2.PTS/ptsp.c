f83d77663bd56effda7d62743d0ee2f5ee9246f0
GoeunLee <21800500@handong.edu>
2020-07-29 11:36:16 +0900

    파일정리
    TSP 알고리즘 참고 : https://shoark7.github.io/programming/algorithm/introduction-to-tsp-and-solve-with-exhasutive-search
    permutation 참고 : https://twpower.github.io/62-permutation-by-recursion

--- 2.PTS/ptsp.c
+++ 2.PTS/ptsp.c
@@ -1,130 +1,75 @@
-/*
-TSP 알고리즘 참고 : https://shoark7.github.io/programming/algorithm/introduction-to-tsp-and-solve-with-exhasutive-search
-permutation 참고 : https://twpower.github.io/62-permutation-by-recursion
-*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
-//#include <error.h>
 #include <math.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <error.h>
 #include <sys/mman.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
-#define N 5
-#define cal_limit 2
+#define N 17
+#define cal_limit 12
 
-typedef struct {
-    int child_path[N];
-} Buffer;
-
-int pipes[2]; //result 전달을 위한 파이프
-int dist[N][N]; //도시간의 거리 저장하는 매트릭스
-float ANS = INFINITY;
+int** pipes; //for transfer result
+int dist[N][N]; //distances between cities
+int best_path[N];
+int ans = 2000000000;
 int** prefix;
 int** path;
-int* best_path;
+int* process_pipe_table;
+int prefixLen = N - cal_limit;
 int prefixCase = 1;
 int temp_count = 0;
+int running_limit = 0;
+int running_proc = 0;
+int process_count = 0;
 int parent_pid;
 
-int find_pid(int* total_pid, int limit);
-void terminate_handler(int sig);
-void permutation(int n, int r, int depth, int* arr);
-void openfile(char filename[]);
-void makePrefix();
-void child_proc(int shm_id,int prefixNum, int prefixLen);
-void parent_proc(int shm_id, char name[],int prefixNum, int* total_pid, int curr);
-void find_path(int start, int last, int child_path[], int tmp_dist, int used[], int visited, float ans);
-
-
-int main(int argc, char* argv[]){
-    if(argc != 3){
-        printf("Wrong input: Enter filename and limit of children processes");
-        return 0;
+void final_print(){
+    //best solution upto the point
+    printf("best solution :\n");
+    for(int i=0;i<N;i++){
+        printf("%d ",best_path[i]);
     }
-    int limit = atoi(argv[2]);
-    pid_t child_pid[limit];
-    parent_pid = getpid();
-    signal(SIGINT, terminate_handler);
-
-    openfile(argv[1]);
-    makePrefix();
-
-    int prefixLen = N - cal_limit;
-    int prefixNum = prefixCase;
-    int curr_pid = 0;
-    int running_pid = 0;
-    int total_pid[limit];
-    for(int i=0;i<limit;i++){
-        total_pid[i]=0;
-    }
-
-    while(prefixNum != 0){ // if still prefix list remain
-        if(running_pid < limit){ //if there is remaining child_pid
-            if (pipe(pipes) != 0) {
-                perror("Error") ;
-                exit(1) ;
-            }
-            curr_pid = find_pid(total_pid, limit);
-            if(curr_pid == -1) continue;
-
-            //make a shared memory
-            char name[32];
-            sprintf(name,"/buffer[%d]",curr_pid);
-            int shm_id = shm_open(name, O_CREAT | O_RDWR, 0666);
-            ftruncate(shm_id,sizeof(Buffer));
-    
-            child_pid[curr_pid] = fork();
-             /*DEBUGGING*/
-            printf("%d pid fork\n",curr_pid);
-            total_pid[curr_pid] = 1;
-             /*DEBUGGING*/
-            for(int i=0;i<limit;i++){
-                printf("main:total_pid[%d]: %d\n",i,total_pid[i]);
-            }
-
-            prefixNum --;
-            running_pid++;
-
-            if(child_pid < 0){
-                printf("Failed to create child process\n");
-                exit(-1);
-            } else if(child_pid > 0){
-                parent_proc(shm_id, name, prefixNum, total_pid, curr_pid);
-            } else {
-                child_proc(shm_id, prefixNum, prefixLen);
-                 /*DEBUGGING*/
-                printf("prefixNum : %d\n",prefixNum);
-            }
+    //total number of checked/covered routes upto the point
+    printf("\nchecked routes :\n");
+    for(int i=0;i<process_count;i++){
+        for (int j=0;j<N;j++) {
+            if(path[i][j]!=-1)
+                printf("%d ",path[i][j]);
         }
+        printf("\n");
     }
-    sleep(5);
-    exit(0);
+    printf("\nlength = %d\n",ans);
 }
 
 void terminate_handler (int sig){
-    if ((sig == SIGINT) && (getpid() == parent_pid)) {
-        //best solution upto the point
-        printf("best solution :\n");
-        for(int i=0;i<N;i++){
-            printf("%d ",best_path[i]);
-        }
-        //total number of checked/covered routes upto the point
-        printf("\nchecked routes :\n");
-        for(int i=0;i<prefixCase;i++){
-            for (int j=0;j<N;j++) {
-                if(path[i][j]!=-1)
-                    printf("%d ",path[i][j]);
-            }
-        }
-        printf("\nlength = %f\n",ANS);
+    if ((sig == SIGINT) && (getpid() == parent_pid)) { //parent
+        final_print();
         exit(0);
     }
-    exit(0);
+    else{
+        exit(0);
+    }
+}
+
+void openFile(char filename[]){
+    FILE * fp = fopen(filename, "r");
+    if(fp == NULL){
+        printf("Failed to open %s\n",filename);
+        exit(1);
+    }
+    int t;
+    for (int i = 0 ; i < N ; i++){
+        for (int j = 0 ; j < N ; j++){
+            fscanf(fp, "%d", &t) ;
+            dist[i][j] = t ;
+        }
+    }
+    fclose(fp);
 }
 
 void permutation(int n, int r, int depth,int* arr){
@@ -147,42 +92,11 @@ void permutation(int n, int r, int depth,int* arr){
 
 }
 
-int find_pid(int* total_pid, int limit){
-     /*DEBUGGING*/
-    for(int i=0;i<limit;i++){
-        printf("find_pid:total_pid[%d]: %d\n",i,total_pid[i]);
-    }
-    for(int i=0;i<limit;i++){
-        if(total_pid[i]==0) return i;
-    }
-    return -1;
-}
-
-void openfile(char filename[]){
-    FILE * fp = fopen(filename, "r");
-    if(fp == NULL){
-        printf("Failed to open %s\n",filename);
-        exit(1);
-    }
-
-    int t;
-    for (int i = 0 ; i < N ; i++){
-        for (int j = 0 ; j < N ; j++){
-            fscanf(fp, "%d", &t) ;
-            dist[i][j] = t ;
-        }
-    }
-    fclose(fp);
-}
-
 void makePrefix(){
 
-    //prefixCase구해놓기
     for(int i=N ; i>cal_limit ; i--){
         prefixCase *= i;
     }
-    int prefixLen = N - cal_limit;
-    int prefixNum = prefixCase;
 
     prefix = (int**)malloc(sizeof(int*)* prefixCase);
     for(int i=0;i<prefixCase;i++){
@@ -193,121 +107,182 @@ void makePrefix(){
     for(int i=0;i<prefixCase;i++){
         path[i]=(int*)malloc(sizeof(int)*N);
         for(int j=0;j<N;j++){
-            path[i][j]=-1;
+            path[i][j]=-1; //initialize path with -1
         }
     }
 
-    best_path = (int*)malloc(sizeof(int)*N);
+    pipes = (int**)malloc(sizeof(int*)* prefixCase);
+    for(int i=0;i<prefixCase;i++){
+        pipes[i]=(int*)malloc(sizeof(int)*2);
+    }
+
+    process_pipe_table = (int*)malloc(sizeof(int*)* prefixCase);
+    for(int i=0;i<prefixCase;i++){
+        process_pipe_table[i] = -1; //initialize process_pipe_table with -1
+    }
 
     int arr[N];
     for(int i=0;i<N;i++) arr[i]=i;
 	permutation(N, prefixLen, 0, arr);
 }
 
-void child_proc(int shm_id,int prefixNum, int prefixLen){
-    /*
-    sleep(1); // wait for the parent to initialize the shared buffer
-    close(pipes[0]);
-    Buffer *buffer = (Buffer*)mmap(0,sizeof(Buffer),PROT_READ|PROT_WRITE,MAP_SHARED,shm_id,0);
-    */
-    //handle prefix
-    int child_prefix[prefixLen];
-    for(int i=0;i<prefixLen;i++){
-        child_prefix[i] = prefix[prefixNum][i];
-    }
+void find_path(int start, int last, int tmp_dist, int curr, int* length, int used[], int child_path[], int* best_child_path){
 
-    float ans = INFINITY;
-    int visited = 0;
-    int used[N] = {0};
-    int child_path[N] = { [0 ... N-1] = -1 };
-    int last = prefix[prefixNum][prefixLen-1];
-    int start = prefix[prefixNum][0];
-    int tmp_dist = 0;
-
-    //find tmp_dist of prefix
-    for(int i=0;i<prefixLen-2;i++){
-        tmp_dist += dist[i][i+1];
-    }
-    //initialize array for find_path
-    for(int i=0;i<prefixLen;i++){
-        used[prefix[prefixNum][i]]=1;
-        child_path[i]=prefix[prefixNum][i];
-    }
-    /*DEBUGGING*/
-    printf("prefixNum : %d\n",prefixNum);
-    for(int i=0;i<N;i++){
-        printf("used[%d]: %d \n",i,used[i]);
-    }
-    for(int i=0;i<prefixLen;i++){
-        printf("child_path[%d]: %d\n",i,child_path[i]);
-    }
-     /*DEBUGGING*/
-    /*
-    find_path(start, last, child_path, tmp_dist, used, visited, ans);
-
-    //send ans using pipe to parent
-    char buf[32];
-    sprintf(buf,"%f",ans);
-    write(pipes[1], buf, 31);
-
-    //send this path using shm
-    for(int i=0;i<N;i++){
-        buffer->child_path[i] = child_path[i];
-    }
-
-    for(int i=0;i<N;i++){
-        path[prefixNum][i] = child_path[i];
-    }
-
-    munmap(buffer,sizeof(Buffer));
-    */
-    exit(0);
-}
-
-void parent_proc(int shm_id, char name[],int prefixNum, int* total_pid, int curr){
-    int exit_code;
-    close(pipes[1]);
-    Buffer* buffer = (Buffer*)mmap(0,sizeof(Buffer),PROT_READ,MAP_SHARED,shm_id,0);
-    
-    //wait til child process is terminated
-    wait(&exit_code);
-     /*DEBUGGING*/
-/* 
-    //get info from child
-    char buf[32];
-    read(pipes[0], buf, 31);
-    int ans = atof(buf);
-    if(ans < ANS){
-        ANS = ans;
-        for(int i=0;i<N;i++){
-            best_path[i] = buffer->child_path[i];
-        }
-    }
-    close(pipes[0]);
-
-    shm_unlink(name);
-*/
-    total_pid[curr] = 0;
-
-}
-
-void find_path(int start, int last, int child_path[], int tmp_dist, int used[], int visited, float ans){
     int FLAG = 1;
     for(int i=0;i<N;i++){
-        if(used[i] != 0) FLAG = 0;
+        if(used[i] == 0) FLAG = 0;
     }
-    if (FLAG==1){ //모든 도시를 모두 거쳤을 경우
+    if (FLAG==1){ 
         int return_home_dist = dist[last][start];
-        ans = (ans < tmp_dist + return_home_dist) ? ans : tmp_dist+return_home_dist;
+        if(*length > (tmp_dist + return_home_dist)){
+            *length = tmp_dist + return_home_dist;
+            for(int i=0;i<N;i++){
+                best_child_path[i]=child_path[i];
+            }
+        }
     }
     else{
         for(int left=0;left<N;left++){
             if(used[left]==0){ //loop for left cities
+
                 used[left]=1;
-                child_path[visited]=left;
-                visited++;
-                find_path(start,left,child_path,tmp_dist+dist[last][left], used, visited, ans);
+                child_path[curr]=left;
+                curr++;
+
+                find_path(start,left,tmp_dist+dist[last][left], curr, length, used, child_path, best_child_path);
+                
+                used[left]=0;
+                child_path[curr]=0;
+                curr--;
             }
         }
     }
+    
+}
+
+void child_proc(int this_process_count){
+    close(pipes[this_process_count][0]);
+    
+    int start = prefix[this_process_count][0];
+    int last = prefix[this_process_count][prefixLen-1];
+    int curr = prefixLen;
+    int used[N];
+    int child_path[N];
+    int best_child_path[N];
+    for(int i=0;i<N;i++){
+        used[i]=child_path[i]=0;
+    }
+    for(int i=0;i<prefixLen;i++){
+        child_path[i]=prefix[this_process_count][i];
+        used[child_path[i]]=1;
+    }
+
+    int tmp_dist = 0;
+    for(int i=0;i<prefixLen-1;i++){
+        tmp_dist += dist[child_path[i]][child_path[i+1]];
+    }
+    int length = 2000000000;
+    find_path(start, last, tmp_dist, curr, &length, used, child_path, &best_child_path[0]);
+
+    if(length < ans){
+        for(int i=0;i<N;i++){
+            best_path[i] = child_path[i];
+        }
+    }
+
+    char message[256];
+    char buffer[32];
+    sprintf(message,"%d %d ",this_process_count,length);
+    for(int i = 0; i < N; ++i){
+        sprintf(buffer,"%d ",best_child_path[i]);
+        strcat(message,buffer);
+    }
+    write(pipes[this_process_count][1], message, 256);
+
+    for(int i=0;i<N;i++){
+        used[i]=child_path[i]=0;
+    }
+    tmp_dist = 0;
+    length = 2000000000;
+    close(pipes[this_process_count][1]);
+    exit(0);
+}
+void sigchld_handler(int sig){
+    int exitcode;
+    int this_process_count = -1;
+    int this_pipe_count;
+    int length;
+    int i = 0;
+    char message[256];
+
+    pid_t child = wait(&exitcode) ;
+    for(int j=0;j<prefixCase;j++){
+        if(process_pipe_table[j]==child){
+            this_pipe_count = j;
+            break;
+        }
+    }
+    close(pipes[this_pipe_count][1]);//read mode
+    read(pipes[this_pipe_count][0], message, 256);
+
+    char *ptr = strtok(message, " ");
+    sscanf(ptr,"%d",&this_process_count);
+    ptr = strtok(NULL, " ");
+    sscanf(ptr,"%d",&length);
+    ptr = strtok(NULL, " ");
+
+    while (ptr != NULL && i<N){
+        sscanf(ptr,"%d",&path[this_process_count][i]);
+        ptr = strtok(NULL, " ");
+        i++;
+    }
+
+    if(length < ans){
+        ans = length;
+        for(int i=0;i<N;i++){
+            best_path[i] = path[this_process_count][i];
+        }
+    }
+
+    close(pipes[this_pipe_count][0]);
+    running_proc--;
+}
+
+int main(int argc, char* argv[]){
+    running_limit = atoi(argv[2]);
+    openFile(argv[1]);
+    parent_pid=getpid();
+    signal(SIGCHLD, sigchld_handler);
+    signal(SIGINT, terminate_handler);
+
+    makePrefix();
+    
+    while(process_count<prefixCase){
+        pid_t child_pid;
+        if(pipe(pipes[process_count]) != 0){
+            perror("Error") ;
+            exit(1) ;
+        }
+        if(running_proc == running_limit){
+            wait(0);
+        }
+        if(running_proc < running_limit){
+            running_proc++;
+            process_count++;
+            child_pid = fork();
+            if (child_pid < 0){
+                printf("Failed to make child process\n");
+                exit(1);
+            }
+            else if(child_pid > 0){
+                process_pipe_table[process_count-1]=child_pid;
+            }
+            else{
+                child_proc(process_count-1);
+            }
+        }
+    }
+    sleep(10);
+    final_print();
+    exit(0);
 }
